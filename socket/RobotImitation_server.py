be2542df64455e833c5b73587e517890a7d16daf
Guanghan Pan <gpan@middlebury.edu>
2019-06-04 18:36:13 +0000

    added load, save and change velocity scaling

--- socket/RobotImitation_server.py
+++ socket/RobotImitation_server.py
@@ -3,6 +3,7 @@
 import asyncio
 import pickle
 import sys
+import time
 from pyquaternion import Quaternion
 from xamla_motion.data_types import CartesianPath, JointPath, Pose, JointValues
 from xamla_motion.motion_client import EndEffector, MoveGroup
@@ -10,38 +11,44 @@ from xamla_motion.utility import register_asyncio_shutdown_handler
 import socket
 
 HOST = ''  # Standard loopback interface address (localhost)
-PORT = 30006        # Port to listen on (non-privileged ports are > 1023)
-PORT2 = 30007
+PORT = 50001        # Port to listen on (non-privileged ports are > 1023)
 
 
 def newPath(move_group):
     i = 0;
     new_path = []
     while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    command = data.decode("utf-8")
-                    if command == 'end':
-                        new_path = JointPath(new_path[0].joint_set,new_path)
-                        conn.sendall(b"path successfully created, now enter your command")
-                        return new_path
-                    else:
-                        new_path.append(move_group.get_current_joint_positions())
-                        i+=1
-                        conn.sendall(b"Move Robot Arm to viewpoint %d, and press Enter to continue..." %i)
+        time.sleep(0.5)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                command = data.decode("utf-8")
+                if command == 'end':
+                    new_path = JointPath(new_path[0].joint_set,new_path)
+                    conn.sendall(b"path successfully created, now enter your command")
+                else:
+                    new_path.append(move_group.get_current_joint_positions())
+                    i+=1
+                    conn.sendall(b"Move Robot Arm to viewpoint %d, and press Enter to continue..." %i)
+            conn.close()
+        s.close()
+        if command == 'end':
+            return new_path
 
 async def joint_moves(move_group,path):
     velocity = 0.05
+    quit = False
     while True:
+        time.sleep(0.5)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT2))
+            s.bind((HOST, PORT))
             s.listen()
             conn, addr = s.accept()
             with conn:
@@ -53,7 +60,7 @@ async def joint_moves(move_group,path):
                     if instruction.isdigit():
                         view_points = path.points
                         num = int(instruction)
-                        if num <= len(view_points):
+                        if num < len(view_points):
                             print('--- Going to Viewpoint %d---' %num)
                             await move_group.move_joints_collision_free(view_points[num], velocity_scaling = velocity)
                             conn.sendall(b'success')
@@ -66,25 +73,33 @@ async def joint_moves(move_group,path):
                         await move_group.move_joints(path, velocity_scaling = velocity)
                         print("--- Finished ---")
                         conn.sendall(b'success')
-                    elif instruction == "v":
-                        new_velocity = float(input('current velocity is: %s, enter new velocity (float point number range 0-1):' %velocity))
-                        if new_velocity>0 and new_velocity<=1:
-                            velocity = str(new_velocity)
-                        else:
-                            conn.sendall(b'please enter a valid number')
-                    elif instruction == "s":
-                        name = input("Enter the name of the path: ")
-                        file_path = open('%s.obj'%name, 'wb') 
-                        pickle.dump(path, file_path)
-                        print('saved as %s.obj\n'%name)
                     elif instruction == "q":
-                        break
+                        quit = True
+                        conn.sendall(b'bye')
                     else:
-                        conn.sendall(b"""usage: - e: execute the path from the beginning
+                        split = instruction.split(' ')
+                        if len(split)==2 and split[0]=='v':
+                            new_velocity = float(split[1])
+                            if new_velocity>0 and new_velocity<=1:
+                                velocity = str(new_velocity)
+                                message = 'Successfully set velocity to '+velocity
+                                conn.sendall(message.encode("utf-8"))
+                            else:
+                                conn.sendall(b'please enter a valid number, 0-1')
+                        elif len(split)==2 and split[0]=='s':
+                            name = split[1]
+                            file_path = open('%s.obj'%name, 'wb') 
+                            pickle.dump(path, file_path)
+                            message = 'Successfully saved path as %s'%name
+                            conn.sendall(message.encode("utf-8"))
+                        else:
+                            conn.sendall(b"""usage: - e: execute the path from the beginning
        - number: go to specified viewpoint
        - v: change the velocity
        - s: save the path to local directory
        - q: quit the program""")
+        if quit:
+            return
     
 if __name__ == '__main__':
 
@@ -94,29 +109,42 @@ if __name__ == '__main__':
     end_effector = move_group.get_end_effector()
 
     pathName = ''
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        print('waiting for connection')
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                pathName = data.decode("utf-8")
-                if pathName == 'new':
-                    conn.sendall(b'Move Robot Arm to viewpoint 0, and press Enter to continue...')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen()
+    print(s.gettimeout())
+    print('waiting for connection')
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while 1:
+            data = conn.recv(1024)
+            if not data:
+                break
+            pathName = data.decode("utf-8")
+            if pathName == 'new':
+                conn.sendall(b'Move Robot Arm to viewpoint 0, and press Enter to continue...')
+            else:
+                try:
+                    file_path = open(pathName, 'rb') 
+                except:
+                    message = 'An error occurs while loading %s'%pathName
+                    conn.sendall(message.encode("utf-8"))
+                    exit()
                 else:
-                    conn.sendall(b'foo2')
-                    # path = loadPath(pathName)
-    
+                    message = 'Sucessfully loaded %s'%pathName
+                    conn.sendall(message.encode("utf-8"))
+                    path = pickle.load(file_path)
+        conn.close()
+    s.close()
+
     if pathName == 'new':
         path = newPath(move_group)
                         
     loop = asyncio.get_event_loop()
     register_asyncio_shutdown_handler(loop)
+    
 
     try:
         loop.run_until_complete(joint_moves(move_group,path))
